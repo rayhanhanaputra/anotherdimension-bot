db2ece04bd1fb1e6e279e92012e13092cb531579
Rayhan Hanaputra <rayhanrhanaputra@outlook.com>
2021-03-25 19:35:23 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(msg):
     bot.send_chat_action(msg.chat.id, 'typing')
-    bot.send_message(msg.chat.id,'Selamat datang di Ticket Booth PENTAS SENI 2021 - Another Dimension.\n Ketik /daftar untuk mendapatkan tiket sekarang juga!')
+    bot.send_message(msg.chat.id,'Selamat datang di Ticket Booth PENTAS SENI 2021 - Another Dimension.\nKetik /daftar untuk mendapatkan tiket sekarang juga!')
 
 @bot.message_handler(commands=['daftar'])
 def qr_code_handler(message):    
@@ -17,11 +17,14 @@ def qr_code_handler(message):
     bot.register_next_step_handler(sent, qrcode)
 
 def qrcode(message):
-    url=pyqrcode.create(message.text)
-    url.png('TICKET-QR-CODE.png',scale=15)
-    bot.send_chat_action(message.chat.id, 'upload_document')
-    bot.send_document(message.chat.id,open('TICKET-QR-CODE.png','rb' ))
-
+    if(len(message)!=10 and messange.isdigit()):
+	url=pyqrcode.create(message.text)
+	url.png('TICKET-QR-CODE.png',scale=15)
+	bot.send_chat_action(message.chat.id, 'upload_document')
+	bot.send_document(message.chat.id,open('TICKET-QR-CODE.png','rb' ))
+    else:
+	bot.send_message(message.chat.id, "Input salah :(")
+    
 while True:
 	bot.polling()
 	time.sleep(1)
