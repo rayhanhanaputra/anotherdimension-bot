3734b1d24f7f111f1ca8bfa566635468bb78e603
Rayhan Hanaputra <rayhanrhanaputra@outlook.com>
2021-05-15 11:10:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,16 @@ def start_message(msg):
     bot.send_chat_action(msg.chat.id, 'typing')
     bot.send_message(msg.chat.id,'Selamat datang di Ticket Booth PENTAS SENI 2021 - Another Dimension.')
     sent = bot.send_message(msg.chat.id,'Masukkan NPM Abang/Mba untuk mendapatkan tiket!')
-    while sent.text.isnumeric() == False:
-        sent = bot.send_message(msg.chat.id,'Mohon izin untuk memasukkan NPM dengan benar...')
+    bot.register_next_step_handler(sent, qrcode)
+	
+def ulang(msg):
+    bot.send_chat_action(msg.chat.id, 'typing')
+    sent = bot.send_message(msg.chat.id,'Mohon izin untuk memasukkan NPM Abang/Mba dengan benar!')
     bot.register_next_step_handler(sent, qrcode)
 
 def qrcode(message):
+    while message.text.isnumeric() == False:
+        bot.register_next_step_handler(ulang)
     url=pyqrcode.create(message.text)
     url.png('TICKET-QR-CODE.png',scale=15)
     bot.send_chat_action(message.chat.id, 'upload_document')
