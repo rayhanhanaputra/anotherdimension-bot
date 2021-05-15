de46f7d19693fa2d30ff0bac649edfe3b8207806
Rayhan Hanaputra <rayhanrhanaputra@outlook.com>
2021-05-15 11:41:29 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,8 +18,10 @@ def ulang(msg):
     bot.register_next_step_handler(sent, qrcode)
 
 def qrcode(message):
-    #if message.text.isnumeric() == False or len(message.text) != 10:
-        #bot.register_next_step_handler(message.text, ulang)
+    while message.text.isnumeric() == False or len(message.text) != 10:
+        bot.send_chat_action(message.chat.id, 'typing')
+        sent = bot.send_message(message.chat.id,'Mohon izin untuk memasukkan NPM Abang/Mba dengan benar!')
+        bot.register_next_step_handler(sent, qrcode)
     url=pyqrcode.create(message.text)
     url.png('TICKET-QR-CODE.png',scale=15)
     bot.send_chat_action(message.chat.id, 'upload_document')
