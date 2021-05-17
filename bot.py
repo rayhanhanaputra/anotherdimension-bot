43438d0f96ba3f06a5c9498d98d58b1175cd5949
Rayhan Hanaputra <rayhanrhanaputra@outlook.com>
2021-05-17 22:28:20 +0700

    updet

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import telebot
 import pyqrcode
 import time
 import csv
+import hashlib
 
 token="1776988419:AAF4xYEKQUcupQ3J7IKCqxyARF75H5PfpO0"
 bot=telebot.TeleBot(token)
@@ -21,13 +22,13 @@ with open('data.csv') as csv_file:
 @bot.message_handler(commands=['start'])
 def start_message(msg):
     bot.send_chat_action(msg.chat.id, 'typing')
-    bot.send_message(msg.chat.id,'Selamat datang di Ticket Booth PENTAS SENI 2021 - Another Dimension.')
-    sent = bot.send_message(msg.chat.id,'Masukkan NPM Abang/Mba untuk mendapatkan tiket!')
+    bot.send_message(msg.chat.id,'Selamat datang di Bot Telegram Ticket Generator PENTAS SENI 2021 SEMBAGI ARUTALA Karya Luminous!')
+    sent = bot.send_message(msg.chat.id,'Masukkan NPM Abang/Mba untuk mendapatkan tiket sekarang juga!')
     bot.register_next_step_handler(sent, qrcode)
 	
 def ulang(msg):
     bot.send_chat_action(msg.chat.id, 'typing')
-    yow = bot.send_message(msg.chat.id,'Mohon izin untuk memasukkan NPM Abang/Mba dengan benar!')
+    yow = bot.send_message(msg.chat.id,'Mohon izin untuk memasukkan NPM Abang/Mba dengan benar...')
     bot.register_next_step_handler(yow, qrcode)
 
 def qrcode(message):
@@ -37,11 +38,12 @@ def qrcode(message):
             kebenaran=1
     if kebenaran==0:
         return ulang(message)
+    hash_object = hashlib.sha512(message.text.encode())
+    hashnya = hash_object.hexdigest()
     url=pyqrcode.create(message.text)
-    url.png('TICKET-QR-CODE.png',scale=15)
+    url.png(hashnya+'.png',scale=15)
     bot.send_chat_action(message.chat.id, 'upload_document')
-    pesan = "TICKET-QR-CODE.png"
-    bot.send_document(message.chat.id,open(pesan,'rb' ))
+    bot.send_document(message.chat.id,open(hashnya+'.png','rb'))
     bot.send_message(message.chat.id,'Tunjukkan QR Code ini saat melakukan absensi!\nTerima kasih Bang/Mba')
     bot.send_message(message.chat.id,'Ketik /start untuk membuat tiket lagi')
     
